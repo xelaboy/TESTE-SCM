7d04eef2171105d25283817161fd8dc895bb71d1
AlexLuz <xelaluz@hotmail.com>
2021-03-20 09:07:15 -0300

    First Commit

---
+++ gitLib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#
+clonaRepo()              ##### Function to clone a repo ###
+{
+REPO="$1"
+USER="$2"
+BRAN="$3"
+#
+git clone -b ${BRAN} https://github.com/${USER}/${REPO}
+#
+}
+#
+#
+clonaRepo $1 $2 $3
+#
+#
+#
